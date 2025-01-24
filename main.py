520925de8ff52f9d4ec2017a0cd44ff518244471
Javier Solana <javier.solana@cabify.com>
2025-01-24 20:52:24 +0100

    prompts improvements

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 GITLAB_API_TOKEN = os.environ.get("GITLAB_API_TOKEN")
 GITLAB_API_BASE_URL = os.environ.get("GITLAB_API_BASE_URL")
 RAG_API_TOKEN = os.environ.get("RAG_API_TOKEN")
-RAG_URL = os.environ.get("RAG_URL", "http://localhost:9000/backstage-ai/api/query")
+RAG_URL = os.environ.get("RAG_URL")
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -66,17 +66,40 @@ async def gitlab_webhook(request: Request):
 
         headers = {
             "Content-Type": "application/json",
-            "X-API-KEY": RAG_API_TOKEN,  # Usamos la variable RAG_API_TOKEN
+            "X-API-KEY": RAG_API_TOKEN,
         }
 
         query = f"""
-            Based on the following issue description and list of labels:
+            Based on the following issue description:
             Description: {description}
-            Labels: {', '.join(labels)}
 
-            Provide a helpful comment for the task. If you think it is relevant, include a GitLab label command with the labels you believe should be associated with this issue.
+            And the available labels in the project: {', '.join(labels)}
+
+            Please suggest the label(s) that best fit this issue, considering its description and the context provided. For your response, use the format: /label ~"label_name". If you believe multiple labels are relevant, include all of them, separated by commas.
+            As a fallback use /label ~"Maintenance"
             """
 
+        data = {
+            "query": query,
+            "system_prompt": "ou are an expert in issue classification, skilled at analyzing problem descriptions and identifying the most relevant labels for a given task. Use your expertise to make the best classification based on the available information.",
+            "temperature": 0,
+            "context": [],
+        }
+
+        response = requests.post(RAG_URL, headers=headers, json=data)
+        suggested_labels = '/label ~"Maintenance"'
+        if response.status_code == 200:
+            response_data = response.json()
+            answer = response_data.get("answer")
+            if "/label ~" in answer:
+                suggested_labels = answer
+
+        query = f"""
+            Based on the following issue description:
+            Description: {description}
+
+            Provide a helpful comment for the task based on your knowledge to solve it or improve the description.
+            """
         data = {
             "query": query,
             "system_prompt": "You are an AI assistant trying to help as much as possible.",
@@ -89,36 +112,34 @@ async def gitlab_webhook(request: Request):
         if response.status_code == 200:
             response_data = response.json()
             answer = response_data.get("answer")
-            sources = response_data.get("sources", [])
-            sources_info = "\n".join(
-                [
-                    f"Source name: {source['name']}, URL: {source['url']}"
-                    for source in sources
-                ]
-            )
-            comment_body = f"""
-                Hi!
-                {answer}
-
-                Sources:
-                {sources_info}
-            """
-
-            comment_url = (
-                f"{GITLAB_API_BASE_URL}/projects/{project_id}/issues/{issue_iid}/notes"
-            )
-            comment_data = {"body": comment_body}
-            comment_response = requests.post(
-                comment_url, headers=headers, json=comment_data
-            )
-            if comment_response.status_code != 201:
-                raise HTTPException(
-                    status_code=comment_response.status_code,
-                    detail="Failed to create comment",
+            if "try a different prompt" not in answer:
+                sources = response_data.get("sources", [])
+                sources_info = "\n".join(
+                    [f"- [{source['name']}]({source['url']})" for source in sources]
                 )
+                comment_body = f"""{answer}\n\n### Sources\n\n{sources_info}\n\n{suggested_labels}"""
+                headers = {"Private-Token": GITLAB_API_TOKEN}
+                comment_url = f"{GITLAB_API_BASE_URL}/projects/{project_id}/issues/{issue_iid}/notes"
+                comment_data = {"body": comment_body}
+                comment_response = requests.post(
+                    comment_url, headers=headers, json=comment_data
+                )
+                if comment_response.status_code != 201:
+                    raise HTTPException(
+                        status_code=comment_response.status_code,
+                        detail="Failed to create comment",
+                    )
+            else:
+                logger.info(f"Ignored response, cause it is not valid: {answer}")
         else:
             print(f"Error: {response.status_code}, {response.text}")
 
         return {"message": "Webhook processed and comment created successfully"}
 
     raise HTTPException(status_code=400, detail="Event not handled")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
